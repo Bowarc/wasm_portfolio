992781609cebda6d3fb218bdde3db37656d80aac
Bowarc <yuyeaaaahhhh@laposte.net>
2024-04-13 20:31:24 +0200

    Start of 'veille technologique' on WASM

--- front/src/utils.rs
+++ front/src/utils.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 
+use gloo::console::log;
 use js_sys::Date;
 
 #[derive(Debug)]
@@ -38,4 +39,38 @@ pub fn time_since(date: Date) -> String {
     }
 
     s(interval, "second")   
+}
+
+
+pub fn add_script(path: &str, id: &str) {
+    let Some(window) = web_sys::window() else{
+        log!(format!("Could not set script {id} due to: Could not get the window"));
+        return;
+    };
+    let Some(document) = window.document() else{
+        log!(format!("Could not set script {id} due to: Could not get the document"));
+        return;
+    };
+
+    let script = document.create_element("script").unwrap();
+    script.set_attribute("src", path).unwrap();
+    script.set_attribute("defer", "").unwrap();
+    script.set_id(id);
+    document.body().unwrap().append_child(&script).unwrap();
+}
+
+pub fn remove_script(id: &str) {
+    let Some(window) = web_sys::window() else{
+        log!(format!("Could not remove script {id} due to: Could not get the window"));
+        return;
+    };
+    let Some(document) = window.document() else{
+        log!(format!("Could not remove script {id} due to: Could not get the document"));
+        return;
+    };
+
+    if let Some(script_element) = document.get_element_by_id(id) {
+        let parent_node = script_element.parent_node().unwrap();
+        parent_node.remove_child(&script_element).unwrap();
+    }
 }
